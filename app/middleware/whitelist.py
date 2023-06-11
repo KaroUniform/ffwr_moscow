e2fb401f73b6bf2ca86057cb229e283b52be6f8f
KaroUniform <snow.leopard.111@yandex.ru>
2023-06-12 02:00:52 +0300

    🐛 Rewrite WhitelistMessageMiddleware

--- app/middleware/whitelist.py
+++ app/middleware/whitelist.py
@@ -1,21 +1,26 @@
 from typing import Callable, Dict, Any, Awaitable
-from models.jaynecobbdatabase import Chats, Users, Roles
+from models.jaynecobbdatabase import Chats, Users, Roles, UsersToChats
 
 from aiogram import BaseMiddleware
 from aiogram.types import Message
 
 class WhitelistMessageMiddleware(BaseMiddleware):
+    
+    allowed_chat_cached = [chat.chat_id for chat in Chats.select(Chats.chat_id)]
+    owner_role = Roles.get(Roles.name == 'owner')
+    
     async def __call__(
         self,
         handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
         event: Message,
         data: Dict[str, Any]
     ) -> Any:
-        allowed_chats = list(Chats.select(Chats.chat_id))
-
-        user = Users.get(Users.user_id == event.from_user.id)
         
-        admin_role = Roles.get(Roles.name == 'admin')
-        if (event.chat.id in allowed_chats) or (user.role_id >= admin_role.role_id):
+        
+        is_owner = UsersToChats.select().where(
+            (UsersToChats.user == event.from_user.id) & (UsersToChats.role == self.owner_role.role_id)
+        ).count()
+        
+        if (event.chat.id in self.allowed_chat_cached) or is_owner:
             return await handler(event, data)
             
